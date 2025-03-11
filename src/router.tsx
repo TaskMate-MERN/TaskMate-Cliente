c8564ea6478d45c3a0456a71295b5ef2dcff8b35
BlueVelociraptor <bluevelociraptor1555@gmail.com>
2025-03-10 21:00:18 -0500

    Reorganizacion de rutas en router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthLayout, MainLayout } from "./layouts"
-import { ProjectsView } from "./views/main";
-import { LoginView } from "./views/admin";
 import Login from "./components/admin/Login/Login";
 import TheDashboard from "./components/Dashboard/Dashboard";
 
@@ -11,8 +9,7 @@ const router = () => {
             <Routes>
                 <Route path="/auth" element={<AuthLayout />}>
                     {/* Aqui pondran todas sus rutas del Login, Register, etc... */}
-                    <Route index element={<LoginView />} />
-                    <Route path="login" element={<Login />} />
+                    <Route index element={<Login />} />
 
 
                 </Route>
@@ -20,7 +17,6 @@ const router = () => {
                 <Route path="/" element={<MainLayout />}>
                     {/* Aquí pondran todas las rutas de la app, como las vistas de los proyectos, tareas, etc... */}
                     <Route index element={<TheDashboard />} />
-                    <Route index element={<ProjectsView />} />
                 </Route>
             </Routes>
         </BrowserRouter>
